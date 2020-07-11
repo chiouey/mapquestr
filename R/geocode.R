96b20b0bb9cf65cc0f1d0454fff36135b79f34ce
chiouey <31549973+chiouey@users.noreply.github.com>
2020-07-11 16:01:45 -0400

    reverse geocode

--- R/geocode.R
+++ R/geocode.R
@@ -89,6 +89,71 @@ geocode_mapquest_batch <- function(addresses,
 }
 
 
+#' Interface with MapQuest API to reverse geocode addresses
+#'
+#' \code{geocode_mapquest_reverse} creates and executes calls to MapQuest
+#' reverse geocoding API via HTTP GET.
+#'
+#' @param latlon A dataframe including lat lng pairs to reverse geocode.
+#' @param key An individual API authentication key. MapQuest provides
+#' 15,000 free monthly transactions with a non-gated account. An
+#' account can be created here:
+#' https://developer.mapquest.com/plan_purchase/steps/business_edition/business_edition_free/register
+#'
+#' @return A dataframe will be returned with the given latitudes and
+#' longitudes as well as their corresponding addresses.
+#'
+#' @examples
+#' geocode_mapquest_reverse(schools, key = Sys.getenv("MAPQUEST_API_KEY"))
+#'
+#'
+#' @export
+geocode_mapquest_reverse <- function(latlon,
+                                     key) {
+
+
+   output <- tibble::tibble(lat = double(),
+                            lon = double(),
+                            address = character())
+
+   url_anchor <- "http://www.mapquestapi.com/geocoding/v1/reverse?"
+
+   for (i in 1:dim(latlngs)[1]) {
+
+      query <- paste0(url_anchor, "key=", key,
+                      "&outFormat=json&thumbMaps=FALSE&location=",
+                      latlon$lat[i], "%2C", latlon$lon[i])
+
+
+      response <- httr::GET(query)
+
+      if (response$status_code >= 400)
+         stop(paste0("Error ", response$status_code,
+                     ". See Mapquest API status code documentation for more information."))
+
+      response_data <- httr::content(response)
+
+      output <- output %>%
+         dplyr::bind_rows(
+            tibble::tibble(
+            lat = latlon$lat[i],
+            lon = latlon$lon[i],
+            address = paste0(
+               response_data$results[[1]]$locations[[1]]$street, " ",
+               response_data$results[[1]]$locations[[1]]$adminArea5, ", ",
+               response_data$results[[1]]$locations[[1]]$adminArea3, " ",
+               response_data$results[[1]]$locations[[1]]$postalCode
+               )
+            )
+         )
+   }
+
+   return(output)
+
+}
+
+
+
 #' Interface with MapQuest API to geocode addresses
 #'
 #' \code{geocode_mapquest} creates and executes calls to MapQuest
@@ -145,3 +210,4 @@ geocode_mapquest <- function(addresses,
    return(output)
 
 }
+
